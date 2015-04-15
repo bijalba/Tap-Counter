1955ae8f7fc83a57e5422ea2d239dc19f318f9a4
bijalba <bijalba@christophers-MacBook-Pro.local>
2015-04-15 16:34:23 -0500

    Added button

--- Tap Counter/ViewController.swift
+++ Tap Counter/ViewController.swift
@@ -12,11 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Label
         var label = UILabel()
         label.frame = CGRectMake(150,150,60,60)
         label.text = "0"
         
         self.view.addSubview(label)
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(150,250,60,60)
+        button.setTitle ("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        self.view.addSubview(button)
+        
     }
 
 }
